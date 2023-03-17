f65ca7f7a2f45a666b677a62cb93e097e7b401e6
Caio Costa Santos <63621128+Caio-eng-gif@users.noreply.github.com>
2023-03-17 19:04:34 -0300

    Add files via upload

---
+++ Engenharia de Software/Localizacao.java
@@ -0,0 +1,10 @@
+public class Localizacao {
+
+    String setor;
+    String estante;
+
+    public Localizacao(String setor, String estante){
+        this.setor = setor;
+        this.estante = estante;
+    }
+}
